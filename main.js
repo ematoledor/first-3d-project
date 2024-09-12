d5955490175076530c5964aed3291bcb2dfcfa0b
ematol <ematolr@gmail.com>
2024-09-12 16:45:24 -0600

    first commit on first 3d project with vite and three.js

---
+++ main.js
@@ -0,0 +1,13 @@
+import { Theater } from 'lucide-react';
+import * as THREE from 'three';
+
+const canvas:HTMLElement = document.getElementById( elementid: 'canvas');
+
+// 1. Scene
+
+const scene = new THREE.Scene();
+
+scene.background = new THREE.Color( r: '#F0F0F0');
+
+//Camera
+const camera;
